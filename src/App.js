8bd85a9bf2a58ea740c27786dacf45164c294b37
Becky Reamy <rlreamy@umich.edu>
2021-01-15 13:14:46 -0500

    KPMP-2359: Update all libraries to latest

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import appReducer from './reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import ReactGA from 'react-ga';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory }  from 'history';
 import { Route, Switch, Router } from 'react-router-dom';
 import ErrorBoundaryContainer from './components/Error/ErrorBoundaryContainer';
 import Oops from './components/Error/Oops';
@@ -35,7 +35,7 @@ function logPageView(location, action) {
   ReactGA.set({ page: location.pathname + location.search });
   ReactGA.pageview(location.pathname + location.search);
 }
-const history = createHistory();
+const history = createBrowserHistory();
 history.listen((location, action) => {
   logPageView(location, action);
 });
