78b3a4c066432280b44f51989942c4afb8c42d7a
Becky Reamy <rlreamy@umich.edu>
2021-01-18 16:11:14 -0500

    KPMP-2358: Finish look and feel of header

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavLink, NavbarText, NavItem } from 'reactstrap';
 
 class NavBar extends Component {
   render() {
+
     return (
-      <Navbar id="navbar" className="px-1 py-1 fixed-top">
-        <Col xs={6}>
-          <Link to="/" className="navbar-header">
-            <NavbarBrand className="d-flex align-items-center">
-              <img src="img/logo.png" alt="Kidney Tissue Atlas" className="logo" />
-              <span id="title-text" className="ml-2 text-dark">
-                Kidney Tissue Atlas
-              </span>
-            </NavbarBrand>
-          </Link>
-        </Col>
+      <Navbar id="navbar" className="fixed-top px-1 py-1">
+        <NavbarBrand href="/" id="title-text" className="ml-2 text-dark">
+          <img src="img/logo.png" alt="Kidney Tissue Atlas" className="logo" />
+          <span className="ml-2">Kidney Tissue Atlas</span>
+        </NavbarBrand>
+        
+          <Nav className="mr-auto">
+            <NavItem>
+              <NavLink href="/">Dashboard (Home)</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavbarText className="inactive">Analysis</NavbarText>
+            </NavItem>
+            <NavItem className="active">
+              <NavLink href="/explorer">Explorer</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/repository">Repository</NavLink>
+            </NavItem>
+          </Nav>
+        
       </Navbar>
+      
+      
     );
   }
 }
 
 export default NavBar;
+
+
+/* <Navbar id="navbar" className="px-1 py-1 fixed-top">
+          <div className="navbar-header">
+            <NavbarBrand href="/" >
+              <img src="img/logo.png" alt="Kidney Tissue Atlas" className="logo" />
+              <span id="title-text" className="ml-2 text-dark">
+                Kidney Tissue Atlas
+              </span>
+            </NavbarBrand>
+          </div>
+         
+          <Nav className="mr-auto" navbar>
+            <NavLink href="">Dashboard (Home)</NavLink>
+            <NavLink>Analysis</NavLink>
+            <NavLink href="">Explorer</NavLink>
+            <NavLink href="">Repository</NavLink>
+          </Nav>
+          
+      </Navbar> */
