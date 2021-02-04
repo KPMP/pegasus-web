8b7ff44ddcaeac31511882a4988ff7366b4abe5a
Zach Wright <zwright@umich.edu>
2021-02-04 16:25:21 -0500

    Change graphql host

--- src/helpers/ApolloClient.js
+++ src/helpers/ApolloClient.js
@@ -1,7 +1,7 @@
 import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-    uri: 'http://localhost:8080/graphql',
+    uri: 'http://localhost/graphql',
     cache: new InMemoryCache(),
     fetchOptions: {
         mode: 'no-cors',
