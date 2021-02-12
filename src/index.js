15a8dbcbb3c605e50521edd8492bb5f1f004bb26
Zach Wright <zwright@umich.edu>
2021-02-12 10:16:28 -0500

    KPMP-2468: polyfills and shims for IE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@babel/polyfill';
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import './index.css';
 import './react-table.css';
