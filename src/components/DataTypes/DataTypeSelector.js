30a145305712e76650b0133b8a1907db454f1212
Zach Wright <zwright@umich.edu>
2021-02-22 13:21:07 -0500

    KPMP-2442: Viz Layout

--- src/components/DataTypes/DataTypeSelector.js
+++ src/components/DataTypes/DataTypeSelector.js
@@ -3,6 +3,7 @@ import Select from "react-select/async";
 import { Row, Col } from 'reactstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 class DataTypeSelector extends Component {
     constructor(props) {
@@ -24,7 +25,7 @@ class DataTypeSelector extends Component {
     render() {
         return (
             <React.Fragment>
-                <Row xs="12" className=''>
+                <Row xs="12" className='mb-4'>
                     <Col lg="2" className='text-center d-table'>
                         <Select
                             options={[]}
@@ -35,27 +36,28 @@ class DataTypeSelector extends Component {
                         />
                     </Col>
                     <Col lg="2" className='d-table'>
-                        <span className='d-table-cell align-middle'>in:</span>
+                        <span className='d-table-cell'>in:</span>
                         <Select
                             options={[]}
                             onChange={this.handleSelect}
                             value={this.state.inputValue}
                             onInputChange={this.handleInputChange}
-                            className='select d-table-cell align-middle'
+                            className='select pl-2 d-table-cell w-100 pl-2'
                         />
                     </Col>
                     <Col lg="2" className='d-table'>
-                        <label className='d-table-cell align-middle'>in:</label>
+                        <label className='d-table-cell'>in:</label>
                         <Select
                             options={[]}
                             onChange={this.handleSelect}
                             value={this.state.inputValue}
                             onInputChange={this.handleInputChange}
-                            className='select d-table-cell align-middle'
+                            className='select d-table-cell w-100 pl-2'
                         />
                     </Col>
                     <Col lg='6' className=''>
-                        <span className='d-table-cell align-middle pt-1'>(N=100 PARTICIPANTS; 50 HEALTHY REFERENCE; 30 CKD; 30 AKI)<FontAwesomeIcon icon={faQuestionCircle} className='ml-2'/></span>
+                        <span className='d-table-cell pt-1'>(N=100 PARTICIPANTS; 50 HEALTHY REFERENCE; 30 CKD; 30 AKI)<FontAwesomeIcon icon={faQuestionCircle} className='ml-2'/></span>
+                        <span className='d-table-cell pt-1 pl-4'><FontAwesomeIcon icon={faDownload} /></span>
                     </Col>
                 </Row>
             </React.Fragment>
--- src/components/DataTypes/SNRNASeq.js
+++ src/components/DataTypes/SNRNASeq.js
@@ -3,8 +3,6 @@ import { Container, Row, Col } from 'reactstrap';
 import DataTypeSelectorContainer from './DataTypeSelectorContainer';
 import ExpressionXCellType from "../ExpressionTables/ExpressionXCellType";
 import ExpressionXTissueType from "../ExpressionTables/ExpressionXTissueType";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 class SNRNASeq extends Component {
     constructor(props) {
@@ -17,11 +15,6 @@ class SNRNASeq extends Component {
             <div>
                 <Container className='mt-3 rounded border p-3'>
                     <DataTypeSelectorContainer/>
-                    <Row xs='12' className='my-1'>
-                        <Col xs={{ size: 1, offset: 11 }} className='d-flex justify-content-end'>
-                            <FontAwesomeIcon icon={faDownload} />
-                        </Col>
-                    </Row>
                     <Row xs='12' className='mb-4'>
                         <Col md='6'>
                             <h5>UMAP</h5>
