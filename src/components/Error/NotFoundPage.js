b610861cc9c96ebf9d2e44c3082f417ddbd6f6e1
Becky Reamy <rlreamy@umich.edu>
2021-03-01 11:31:06 -0500

    KPMP-2471: Fix layout

--- src/components/Error/NotFoundPage.js
+++ src/components/Error/NotFoundPage.js
@@ -5,14 +5,14 @@ class NotFoundPage extends Component {
     render() {
         return (
             <article className="container" id='not-found-page'>
-                <Row id="not-found-container">
+                <Row id="not-found-container" className='mr-5 p-5'>
                     <Col xs={12} md={4}>
                         <img className='not-found-image' src="img/404-img.svg" alt="Page not found" id="oops-image"/>
                     </Col>
                     <Col xs={12} md={8} className='not-found-text'>
                         <p className="not-found-regular">Sorry. We couldn't find the page you're looking for.</p>
                         <p className="not-found-small">
-                            If you're still having problems, please contact admin@kpmp.org
+                            If you're still having problems, please contact <a href='mailto:admin@kpmp.org'>admin@kpmp.org</a>
                         </p>
                         <p className="oops-button-container">
                         <Button color='primary'
