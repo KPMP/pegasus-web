f4ee3a35e64746a33e93aeb82d69095bb5134f35
Zach Wright <zwright@umich.edu>
2021-04-06 09:38:29 -0400

    Update function

--- src/components/Plots/UMAPPlot.js
+++ src/components/Plots/UMAPPlot.js
@@ -52,9 +52,13 @@ class UMAPPlot extends Component {
         this.setState({plotData: clusterPlotArray, plotAnnotations: annotations});
     };
 
+    onPlotUpdate = (figure, div) => {
+        console.log(figure.data.length);
+    };
+
     render() {
         return (
-            <Plot divId="umapPlot" data={this.state.plotData}
+            <Plot divId="umapPlot" data={this.state.plotData} onUpdate={this.onPlotUpdate}
                   layout={ { annotations: this.state.plotAnnotations, width: 460, showlegend: false,
                       yaxis: { zeroline: false, showgrid: false, showline: true },
                       xaxis: { zeroline: false, showgrid: false, showline: true },
