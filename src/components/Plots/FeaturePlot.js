fbb01646fd6f23bb742d17d7cec321f20570d500
Zach Wright <zwright@umich.edu>
2021-04-06 11:05:34 -0400

    KPMP-2602: isloading during update

--- src/components/Plots/FeaturePlot.js
+++ src/components/Plots/FeaturePlot.js
@@ -11,6 +11,7 @@ class FeaturePlot extends Component {
 
     componentDidUpdate(prevProps) {
         if (this.props.data !== prevProps.data) {
+            this.setState({isLoading: true});
             this.setData(this.props.data);
         }
     }
