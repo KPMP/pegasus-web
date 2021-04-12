dd548bb6e529e84e0a1340d855b89f74c870b992
Zach Wright <zwright@umich.edu>
2021-04-12 14:44:01 -0400

    KPMP-2603: Caching fields

--- src/components/DataViz/DataViz.js
+++ src/components/DataViz/DataViz.js
@@ -39,6 +39,7 @@ class DataViz extends Component {
 
     componentDidUpdate(prevProps) {
         if (this.props.tissueType !== prevProps.tissueType || this.props.dataType !== prevProps.dataType) {
+            console.log("component updated");
             this.setState({ plotData:[], geneExpressionData:[]});
             this.getGeneExpression(this.props.dataType, this.props.selectedConcept.value, "", this.props.tissueType);
             this.getUmapPoints(this.props.dataType, this.props.selectedConcept.value, this.props.tissueType);
