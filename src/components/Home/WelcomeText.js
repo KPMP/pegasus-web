48e3d5fb321eb3dc64ba59faaf8d26549a36699d
Alex Barkell <albarkel@umich.edu>
2021-05-03 16:29:04 -0400

    Update help link to use real link

--- src/components/Home/WelcomeText.js
+++ src/components/Home/WelcomeText.js
@@ -7,7 +7,7 @@ class WelcomeText extends Component {
             <Container className="mt-3 rounded border p-3 shadow-sm">
                 <Row><Col><p><h5>Welcome to the Kidney Tissue Atlas Explorer</h5></p></Col></Row>
                 <Row><Col><p>Here you can search for markers or cell types of interest and view summary data visualizations across the various KPMP 'omics' technologies.</p></Col></Row>
-                <Row><Col><p><a href='/'>Learn more about our data types and methodologies</a></p></Col></Row>
+                <Row><Col><p><a href='https://www.kpmp.org/help-docs/data'>Learn more about our data types and methodologies</a></p></Col></Row>
             </Container>
         );
     }
