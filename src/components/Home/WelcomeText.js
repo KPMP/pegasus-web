b1d5fecd5523792a07fc0e03530b322c19d56541
Zach Wright <zwright@umich.edu>
2021-05-17 14:33:49 -0400

    KPMP-2734: add that thing it wants

--- src/components/Home/WelcomeText.js
+++ src/components/Home/WelcomeText.js
@@ -7,7 +7,7 @@ class WelcomeText extends Component {
             <Container className="mt-3 rounded border p-3 shadow-sm">
                 <Row><Col><p><h5>Welcome to the Kidney Tissue Atlas Explorer</h5></p></Col></Row>
                 <Row><Col><p>Here you can search for markers or cell types of interest and view summary data visualizations across the various KPMP 'omics' technologies.</p></Col></Row>
-                <Row><Col><p><a target='_blank' href='https://www.kpmp.org/help-docs/data'>Learn more about our data types and methodologies</a></p></Col></Row>
+                <Row><Col><p><a rel="noreferrer" target='_blank' href='https://www.kpmp.org/help-docs/data'>Learn more about our data types and methodologies</a></p></Col></Row>
             </Container>
         );
     }
