5dd5a44954a830820217c6b183176d6dac579a15
Alex Barkell <albarkel@umich.edu>
2021-05-28 17:56:13 -0400

    remove need for / in route

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -29,10 +29,10 @@ const NavBar = (props) => {
             <NavItem className={`${window.location.pathname === '/' ? 'active' : ''} px-1`}>
               <NavLink href="/"><span className="nav-text px-1">Dashboard (Home)</span></NavLink>
             </NavItem>
-            <NavItem className={`${window.location.pathname === '/explorer' ? 'active' : ''} px-1`}>
+            <NavItem className={`${window.location.pathname.replaceAll("/", "") === 'explorer' ? 'active' : ''} px-1`}>
               <NavLink href="/explorer"><span className="nav-text px-1">Explorer</span></NavLink>
             </NavItem>
-            <NavItem className={`${window.location.pathname === '/repository' ? 'active' : ''} px-1`}>
+            <NavItem className={`${window.location.pathname.replaceAll("/", "") === 'repository' ? 'active' : ''} px-1`}>
               <NavLink href="/repository"><span className="nav-text px-1">Repository</span></NavLink>
             </NavItem>
           </Nav>
