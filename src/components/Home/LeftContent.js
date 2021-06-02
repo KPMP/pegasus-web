af33b3fd652e7fa80a010af9325249c86c26efae
Alex Barkell <albarkel@umich.edu>
2021-06-02 15:33:49 -0400

    typo fixes

--- src/components/Home/LeftContent.js
+++ src/components/Home/LeftContent.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import { Container, Row, Button } from 'reactstrap';
 
 class LeftContent extends Component {
-    render() {
-        return(
-              <Row>
-                <Container className="mt-3 rounded border p-3 shadow-sm">
-                  <h5>Atlas Explorer</h5>
-                  <div>Search for markers or cell types of interest and view summary data vizualizations across the various KPMP 'omics' technologies.
+  render() {
+    return (
+      <Row>
+        <Container className="mt-3 rounded border p-3 shadow-sm">
+          <h5>Atlas Explorer</h5>
+          <div>Search for markers or cell types of interest and view summary data visualizations across the various KPMP 'omics' technologies.
                   </div>
-                  <div className='mt-3'>
-                    <Button color='primary' href='/explorer' size='lg' block>Go to Explorer</Button>
+          <div className='mt-3'>
+            <Button color='primary' href='/explorer' size='lg' block>Go to Explorer</Button>
+          </div>
+        </Container>
+        <Container className="mt-3 rounded border p-3 shadow-sm">
+          <h5>Atlas Repository</h5>
+          <div>
+            Download raw gene data generated from KPMP participant biopsies and reference tissue samples.
                   </div>
-                </Container>
-                <Container className="mt-3 rounded border p-3 shadow-sm">
-                  <h5>Atlas Repository</h5>
-                  <div>
-                    Download raw gene data generated from KPMP participant biopsies and reference tissue samples.
+          <div className='mt-3'>
+            <Button color='primary' href='/repository' size='lg' block>Go to Repository</Button>
+          </div>
+        </Container>
+        <Container className="mt-3 rounded border p-3 shadow-sm">
+          <h5>KPMP Central Biorepository</h5>
+          <div>
+            Learn more about ancillary study opportunities using KPMP biospecimens and other resources.
                   </div>
-                  <div className='mt-3'>
-                    <Button color='primary' href='/repository' size='lg' block>Go to Repository</Button>
-                  </div>
-                </Container>
-                <Container className="mt-3 rounded border p-3 shadow-sm">
-                  <h5>KPMP Central Biorepository</h5>
-                  <div>
-                    Learn more about ancillary study opprotunities using KPMP biospeceimens and other resources.
-                  </div>
-                  <div className='mt-3'>
-                    <Button color='primary' href='/repository' size='lg' block>Collaborate with us</Button>
-                  </div>
-                </Container>
-              </Row>
-        );
-    }
+          <div className='mt-3'>
+            <Button color='primary' href='/repository' size='lg' block>Collaborate with us</Button>
+          </div>
+        </Container>
+      </Row>
+    );
+  }
 }
 
 export default LeftContent;
