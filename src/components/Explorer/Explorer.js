beb71f35747789854d988d1239d081c6d095894e
Alex Barkell <albarkel@umich.edu>
2021-07-21 16:47:44 -0400

    Clear tissue and data type states

--- src/components/Explorer/Explorer.js
+++ src/components/Explorer/Explorer.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import WelcomeText from './WelcomeText';
 import DataSelectorContainer from './DataSelectorContainer';
 import NephronSchemaCardContainer from './NephronSchemaCardContainer';
-import { setSelectedConcept } from '../../actions/Concepts/conceptActions'
-import { setGene } from '../../actions/Gene/geneActions'
+import { setSelectedConcept } from '../../actions/Concepts/conceptActions';
+import { setGene } from '../../actions/Gene/geneActions';
+import { setTissueType } from '../../actions/TissueType/tissueTypeActions';
+import { setDataType } from '../../actions/DataType/dataTypeActions';
 
 import { selectedConcept } from '../../initialState'
 import { connect } from "react-redux";
@@ -11,6 +13,8 @@ import { connect } from "react-redux";
 class Explorer extends Component {
     render() {
         this.props.setGene("");
+        this.props.setTissueType("");
+        this.props.setDataType("");
         this.props.setSelectedConcept(selectedConcept);
         return (
             <article>
@@ -26,6 +30,12 @@ const mapDispatchToProps = (dispatch, props) =>
 ({
     setGene(gene) {
         dispatch(setGene(gene));
+    },
+    setTissueType(tissueType) {
+        dispatch(setTissueType(tissueType));
+    },
+    setDataType(dataType) {
+        dispatch(setDataType(dataType));
     },
     setSelectedConcept(concept) {
         dispatch(setSelectedConcept(concept))
