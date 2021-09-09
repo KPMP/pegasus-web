dd83b3a5d2bc2da95727d6acf5e43070952c6181
Alex Barkell <albarkel@umich.edu>
2021-09-09 13:14:15 -0400

    Add ga4 tag

--- src/helpers/googleAnalyticsHelper.js
+++ src/helpers/googleAnalyticsHelper.js
@@ -1,7 +1,14 @@
 import ReactGA from 'react-ga';
+import { ReactGA as ReactGA4 } from 'react-ga4';
 
 export const handleGoogleAnalyticsEvent = (category, action, label) => {
-    ReactGA.event ({
+    ReactGA.event({
+        category: category,
+        action: action,
+        label: label
+    });
+
+    ReactGA4.event({
         category: category,
         action: action,
         label: label
