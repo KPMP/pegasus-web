c0f2bc316d2c31a7a5dce87ecc12c663e846cf34
Alex Barkell <albarkel@umich.edu>
2021-11-05 13:25:03 -0400

    add fuzzy url check

--- src/components/Error/NotFoundPage.test.js
+++ src/components/Error/NotFoundPage.test.js
@@ -1,47 +1,75 @@
 import { getBackToHomePath } from './NotFoundPage';
 
 describe('getBackToHomePath', () => {
-    it('should return the path / when given a path of nothing', () => {
+    it('should return the path / when given a path of nothing', async () => {
         const pathname = '/'
         const expectedPathname = '/'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
-    it('should return the path / when not given a path with explorer', () => {
+    it('should return the path / when not given a path with explorer', async () => {
         const pathname = '/foo'
         const expectedPathname = '/'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
-    it('should return the path / when given multiple subpath without explorer', () => {
+    it('should return the path / when given multiple subpath without explorer', async () => {
         const pathname = '/foo/bar'
         const expectedPathname = '/'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
-    it('should return the path /explorer when given a path with just explorer', () => {
+    it('should return the path /explorer when given a path with just explorer', async () => {
         const pathname = '/explorer'
         const expectedPathname = '/explorer'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
-    it('should return the path /explorer when given a path with explorer in it', () => {
+    it('should return the path /explorer when given a path with explorer in it', async () => {
         const pathname = '/explorer/foo'
         const expectedPathname = '/explorer'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
-    it('should return the path / path when explorer not the first subpath', () => {
+    it('should return the path / path when explorer not the first subpath', async () => {
         const pathname = '/foo/explorer'
         const expectedPathname = '/'
-        const resultedPathname = getBackToHomePath(pathname)
+        const resultedPathname = await getBackToHomePath(pathname)
         
         expect(resultedPathname).toBe(expectedPathname)
     });
+    it('should return the path /explorer when given a path with explorer in it but mis-typed', async () => {
+        const pathname = '/xeplorer/foo'
+        const expectedPathname = '/explorer'
+        const resultedPathname = await getBackToHomePath(pathname)
+        
+        expect(resultedPathname).toBe(expectedPathname)
+    });
+    it('should return the path /explorer when given a path with explorer in it but mis-typed', async () => {
+        const pathname = '/xeweplorer/foo'
+        const expectedPathname = '/explorer'
+        const resultedPathname = await getBackToHomePath(pathname)
+
+        expect(resultedPathname).toBe(expectedPathname)
+    });
+    it('should return the path /explorer when given a path with explorer in it but slightly-mis-typed', async () => {
+        const pathname = '/explorerffff/foo'
+        const expectedPathname = '/explorer'
+        const resultedPathname = await getBackToHomePath(pathname)
+
+        expect(resultedPathname).toBe(expectedPathname)
+    }); 
+    it('should return the path / when given a path with explorer in the path but it is seriously mis-typed', async () => {
+        const pathname = '/exqwjeiqwjeplqwoeqiwejoqweiqwejrerffffffff/foo'
+        const expectedPathname = '/'
+        const resultedPathname = await getBackToHomePath(pathname)
+
+        expect(resultedPathname).toBe(expectedPathname)
+    }); 
 });
 
