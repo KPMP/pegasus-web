385657a7af0b86c91ee1355ca15fde80a12674bc
Alex Barkell <albarkel@umich.edu>
2021-11-08 14:36:28 -0500

    wip: not found isExpected

--- src/components/Error/NotFoundPage.js
+++ src/components/Error/NotFoundPage.js
@@ -1,59 +1,17 @@
 import React, { Component } from 'react';
 import { Row, Col, Button } from 'reactstrap';
+import { isExpectedPath } from 'kpmp-common-components';
+ 
+export function getBackToHomePath(pathname) {
+    console.log('isep', isExpectedPath);
+    const expectedPath = 'explorer'
 
-
-export async function fuzzyPathErrors(pathname, expectedPathname){
-    const slicedPath = pathname.split('')
-    const slicedExpectedPath = expectedPathname.split('')
-    const maxTotalErrors = 3
-    let totalErrors = 0
-    let missingLetters = 0
-    let expected = {}
-    // sets up the expected object, used to avoid nested for loop
-    for await (let char of slicedExpectedPath) {
-        if (!expected[char]) {
-            expected[char] = 1;
-        } else {
-            expected[char] = expected[char] + 1;
-        }
-   }
-
-   // counts complete errors and sets up the counting of missing letters
-   for await (let letter of slicedPath) {
-     if(expected[letter]) {
-         expected[letter] = expected[letter] - 1;
-     } else {
-         totalErrors += 1;
-     }
-   }
-    
-  // counts any missing letters from actual path
-  for  (let key in expected) {
-      missingLetters += expected[key]
-  }
-
-  // fuzzy check to see if errors are in-bounds 
-  if(missingLetters < slicedExpectedPath.length / 2 && totalErrors < maxTotalErrors) {
-      return true
-  } else {
-      return false
-  }
-}
-
-
-export async function getBackToHomePath(pathname) {
-        const expectedPath = 'explorer'
-        if(pathname.split('/')[1].indexOf(expectedPath) >= 0) {
-            return '/explorer';
-        }
-        
-        const fuzzyFoundPath = await fuzzyPathErrors(pathname.split('/')[1], expectedPath)
-        if(fuzzyFoundPath){
-            return '/explorer';
-        } else {
-            return '/'
-        }
+    if(isExpectedPath(pathname, expectedPath)) {
+        return '/explorer'
+    } else {
+        return '/'
     }
+}
  
 class NotFoundPage extends Component {
     
@@ -72,7 +30,7 @@ class NotFoundPage extends Component {
                         <p className="oops-button-container">
                         <Button color='primary'
                             className="btn btn-primary"
-                                onClick={async () =>(window.location.href = await getBackToHomePath(window.location.pathname))}
+                                onClick={async () =>(window.location.href = getBackToHomePath(window.location.pathname))}
                         >
                             Back to Home
                         </Button>
