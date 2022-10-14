f1461c9931a2b8b3e27d757db3145114e5f05d17
dert1129 <nathancreger1111@gmail.com>
2022-10-14 15:11:01 -0400

    revert changes

--- src/components/Home/SamplesByDataTypeTableContainer.js
+++ src/components/Home/SamplesByDataTypeTableContainer.js
@@ -1,16 +1,20 @@
 import { connect } from 'react-redux';
 import SamplesByDataTypeTable from './SamplesByDataTypeTable';
-import { setDataTypeAndRedirect, setDataType } from '../../actions/DataType/dataTypeActions'
+import { setDataTypeAndRedirect } from '../../actions/DataType/dataTypeActions'
 import { withRouter } from 'react-router';
+import { setDataType } from '../../actions/DataType/dataTypeActions';
 
 const mapStateToProps = (state, props) =>
 ({
     selectedDataType: state.selectedDataType,
 });
 
-// const mapDispatchToProps = (dispatch, props) =>
-// ({
-    
-// });
+const mapDispatchToProps = (dispatch, props) =>
+({
+    setDataType(concept) {
+        dispatch(setDataType('all'))
+        dispatch(setDataTypeAndRedirect(concept, props));
+    }
+});
 
-export default withRouter(connect(mapStateToProps)(SamplesByDataTypeTable))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SamplesByDataTypeTable))
