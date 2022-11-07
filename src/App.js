5ade225d69667d7cc78a2dbc09dcd1ff8d8d19b5
Alex Barkell <albarkel@umich.edu>
2022-11-07 14:13:34 -0500

    Update ga4 tag

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const saveState = () => {
 };
 
 // *** Get a new tracking Id and add it here *** //
-const GA_TRACKING_ID = 'UA-124331187-10';
+const GA_TRACKING_ID = 'G-64W6E37TQB';
 
 ReactGA4.initialize(GA_TRACKING_ID, { testMode: process.env.NODE_ENV === 'test' });
 
