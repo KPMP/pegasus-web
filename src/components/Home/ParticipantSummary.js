5645ef59727a3a3b805239f94b5f52999e6b32d7
Dert1129 <nathancreger1111@gmail.com>
2023-01-06 14:29:20 -0500

    mount fetchTissueTypeSummaryCounts

--- src/components/Home/ParticipantSummary.js
+++ src/components/Home/ParticipantSummary.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import {Row, Col } from 'reactstrap';
-
+import { fetchTissueTypeSummaryCounts } from '../../helpers/ApolloClient';
 class ParticipantSummary extends Component {
-  state = {
-    text: this.props.text
-  };
+  constructor(props) {
+      super(props);
+      this.state = { akiCount: [], ckdCount: [], hrtCount:[]};
+    }
   componentDidMount() {
-    const {slice} = this.props;
-    if (slice)
-      this.setState({
-        text: this.state.text.slice(slice[0], slice[1])
-      })
+    if(this.props.tissue.type){
+      this.getTissueCounts();
+    }
+  }
+
+  getTissueCounts = () => {
+    fetchTissueTypeSummaryCounts(this.props.tissueType).then((result) => {
+      this.setState({akiCount: result.akiCount})
+      this.setState({ckdCount: result.ckdCount})
+      this.setState({hrtCount: result.hrtCount})
+    })
   }
   render() {
     return (
@@ -18,7 +25,7 @@ class ParticipantSummary extends Component {
       <Col md='4' lg='4'>
           <div className="centered background-light-blue kpmp-color-dark">
               <div>
-                  <span className="font-size-three-rem">117</span>
+                  <span className="font-size-three-rem">{this.getTissueCounts.ckdCount}</span>
               </div>
               <div>
                   <span className="font-size-one-one-half-rem">CKD</span>
