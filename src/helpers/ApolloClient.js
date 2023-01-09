1a667ae9caea78309672d2178199f44ca6641f23
dert1129 <nathancreger1111@gmail.com>
2023-01-09 12:06:04 -0500

    removed console.log

--- src/helpers/ApolloClient.js
+++ src/helpers/ApolloClient.js
@@ -407,7 +407,6 @@ export const fetchTissueTypeSummaryCounts = async () => {
         fetchPolicy: 'cache-first'
     });
     if (response.data && response.data.getTissueTypeSummaryData) {
-        console.log(response.data.getTissueTypeSummaryData);
         return response.data.getTissueTypeSummaryData;
     }else {
         store.dispatch(sendMessageToBackend("Could not retrieve tissue summary: " + response.error));
