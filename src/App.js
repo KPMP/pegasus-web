74f20960ace4361b5fe92a194140a102d9da7837
Zach Wright <zwright@umich.edu>
2023-08-01 11:21:17 -0400

    KPMP-4665: Pass history

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
         <ApolloProvider client={apolloClient}>
           <BrowserRouter basename={packageJson.baseURL} history={history}>
             <ErrorBoundaryContainer>
-              <NavBar app='atlas' />
+              <NavBar app='atlas' history={this.props.history}/>
               <Switch>
                 <Route exact path='/' history={this.props.history}  component={Home} store={store} />
                 <Route exact path='/explorer' component={Explorer} store={store} />
