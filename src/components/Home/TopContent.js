a0f3a181a59476f0076b7f6368f5223032d05729
Nathan Creger <nathancreger1111@gmail.com>
2024-02-23 10:29:05 -0500

    remove unused row import

--- src/components/Home/TopContent.js
+++ src/components/Home/TopContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Col } from 'reactstrap';
 import { handleGoogleAnalyticsEvent } from '../../helpers/googleAnalyticsHelper';
 
 class TopContent extends Component {
