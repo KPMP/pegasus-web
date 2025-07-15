fcbd6e3e110eaf61f9d8b046055359734ecbcdb3
Becky Reamy <rlreamy@umich.edu>
2025-07-15 15:23:01 -0400

    KPMP-6076: Addressing issues with new implementation of ag-grid

--- src/components/ExpressionTables/RegionalTranscriptomicsTable.js
+++ src/components/ExpressionTables/RegionalTranscriptomicsTable.js
@@ -94,8 +94,19 @@ import {Col} from "reactstrap";
 import {
   ModuleRegistry, AllCommunityModule
 } from "ag-grid-community";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
- ModuleRegistry.registerModules([ AllCommunityModule ]);
+ModuleRegistry.registerModules([ AllCommunityModule ]);
+
+const CustomHeader = (props) => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <span>{props.displayName}</span>
+            <FontAwesomeIcon icon={faCircleInfo} style={{ marginLeft: '5px' }} />
+        </div>
+    );
+};
 
 
 class RegionalTranscriptomicsTable extends Component {
@@ -130,27 +141,25 @@ class RegionalTranscriptomicsTable extends Component {
             { headerName: "STD DEVIATION", field: "stdDev", valueFormatter: params => formatNumberToPrecision(params.value, 3), width: 175},
             { headerName: "FOLD CHANGE", field: "foldChange", valueFormatter: params => formatNumberToPrecision(params.value, 3), width: 175,
                 headerTooltip: 'Fold change of a gene is calculated by dividing the average expression of the gene in the segment of interest by its average expression in all other segments being compared.',
-                headerComponentParams: {
-                    icon: 'fa-user',
-                },
+                headerComponent: CustomHeader,
                 sort: 'desc'
             },
             { headerName: "ADJ P VALUE", field: "pValLog10", valueFormatter: params => formatNumberToPrecision(params.value, 3), width: 175,
-                 headerTooltip: 'P value was calculated using a Wilcoxon rank sum test between the expression of the gene in the segment of interest and its expression in all other segments.',
-                headerComponentParams: {
-                    icon: 'fa-cog',
-                }
+                headerTooltip: 'P value was calculated using a Wilcoxon rank sum test between the expression of the gene in the segment of interest and its expression in all other segments.',
+                headerComponent: CustomHeader,
              },
         ];
     }
 
     render() {
+
         return (
             <React.Fragment>
                 <Col lg='12'>
                     <div className="ag-theme-material img-fluid">
                         <AgGridReact columnDefs={this.state.columnDefs} rowData={this.props.data} onGridReady={this.onGridReady}
-                            showGrid={true} domLayout='autoHeight' />
+                            showGrid={true} domLayout='autoHeight'
+                        />
                     </div>
                 </Col>
             </React.Fragment>
