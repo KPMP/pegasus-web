79a12c89564a5849a7266758ac5060ce48037c4e
Becky Reamy <rlreamy@umich.edu>
2025-07-15 15:41:09 -0400

    KPMP-6076: Addressing issues with new implementation of ag-grid

--- src/components/ExpressionTables/RegionalProteomicsTable.js
+++ src/components/ExpressionTables/RegionalProteomicsTable.js
@@ -117,7 +117,7 @@ const CustomHeader = (props) => {
      return (
          <div className='ag-header-cell-text' style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
              <span>{props.displayName}</span>
-             <FontAwesomeIcon icon={faCircleInfo} style={{ marginLeft: '5px' }} />
+             <FontAwesomeIcon className='kpmp-light-blue' icon={faCircleInfo} style={{ marginLeft: '5px' }} />
          </div>
      );
  };
