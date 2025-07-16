00e59191f85e62ae3984656a21f6b08d62b4b313
Becky Reamy <rlreamy@umich.edu>
2025-07-16 15:26:11 -0400

    KPMP-6076: Trying to figure out what is up with sc/sn tables

--- src/helpers/ApolloClient.js
+++ src/helpers/ApolloClient.js
@@ -245,7 +245,7 @@ export const fetchGeneExpression = async (dataType, geneSymbol, cellType, enroll
 
 	if(response.data && response.data.geneExpressionSummary) {
         console.log(response.data)
-		return response.data.data.geneExpressionSummary;
+		return response.data.geneExpressionSummary;
 	} else {
 		store.dispatch(sendMessageToBackend("Could not retrieve gene expression data: " + response.error));
 	}
