ebc7ab147c631e1ef9687c5f18e64e49c6eea396
Becky Reamy <rlreamy@umich.edu>
2025-07-29 13:01:45 -0400

    KPMP-6151: Migrating diffex by cluster table

--- src/components/ExpressionTables/InfoHeader.js
+++ src/components/ExpressionTables/InfoHeader.js
@@ -2,19 +2,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 
-export default (props) => {
-     return (
-        <div class="ag-cell-label-container" role="presentation">
-            <span data-ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" aria-hidden="true"></span>
-            <span data-ref="eFilterButton" class="ag-header-icon ag-header-cell-filter-button" aria-hidden="true"></span>
-            <div data-ref="eLabel" class="ag-header-cell-label" role="presentation">
-                <span data-ref="eText" class="ag-header-cell-text"> <FontAwesomeIcon className='kpmp-light-blue' icon={faCircleInfo} style={{ marginLeft: '5px' }} /></span>
-                <span data-ref="eFilter" class="ag-header-icon ag-header-label-icon ag-filter-icon" aria-hidden="true"></span>
-                <span data-ref="eSortOrder" class="ag-header-icon ag-header-label-icon ag-sort-order" aria-hidden="true"></span>
-                <span data-ref="eSortAsc" class="ag-header-icon ag-header-label-icon ag-sort-ascending-icon" aria-hidden="true"></span>
-                <span data-ref="eSortDesc" class="ag-header-icon ag-header-label-icon ag-sort-descending-icon" aria-hidden="true"></span>
-                <span data-ref="eSortNone" class="ag-header-icon ag-header-label-icon ag-sort-none-icon" aria-hidden="true"></span>
-            </div>
-         </div>
-     );
- };
+ class InfoHeader  {
+     agParams;
+     eGui;
+     eText;
+
+    init(agParams) {
+        const eGui = (this.eGui = document.createElement('div'));
+        eGui.classList.add('customInnerHeader');
+        const textNode = document.createElement('span');
+        this.eText = textNode;
+
+        textNode.textContent = agParams.displayName;
+
+        if (agParams.icon) {
+            const icon = document.createElement('i');
+            icon.classList.add('fa', `${agParams.icon}`);
+            eGui.appendChild(icon);
+        }
+
+        eGui.appendChild(textNode);
+    }
+
+    getGui() {
+        return this.eGui;
+    }
+
+    refresh(params) {
+        this.eText.textContent = params.displayName;
+        return true;
+    }
+}
+
+export default InfoHeader;
