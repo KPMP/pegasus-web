f84bc0154da45778d0b05b00e94e8684826e64ab
Becky Reamy <rlreamy@umich.edu>
2025-07-29 09:35:36 -0400

    KPMP-6151: Migrating diffex by cluster table

--- src/components/ExpressionTables/geneButton.js
+++ src/components/ExpressionTables/geneButton.js
@@ -1,5 +1,5 @@
 export default ({ data }) => {
-    return (<button onClick={() => console.log('Software Launched')}>
+    return (<button onClick={() => console.log('Software Launched')}  className='table-column btn btn-link text-start p-0'>
             {data.gene}
         </button>);
 };
