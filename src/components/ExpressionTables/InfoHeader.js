896a95d383528eac48fc155f6b65e674bcdcfdd7
Becky Reamy <rlreamy@umich.edu>
2025-07-29 10:32:47 -0400

    KPMP-6151: Migrating diffex by cluster table

--- src/components/ExpressionTables/InfoHeader.js
+++ src/components/ExpressionTables/InfoHeader.js
@@ -7,6 +7,10 @@ export default (props) => {
          <div className='ag-header-cell-text' style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
              <span>{props.displayName}</span>
              <FontAwesomeIcon className='kpmp-light-blue' icon={faCircleInfo} style={{ marginLeft: '5px' }} />
+             <span data-ref="eSortOrder" class="ag-header-icon ag-header-label-icon ag-sort-order" aria-hidden="true"></span>
+             <span data-ref="eSortAsc" class="ag-header-icon ag-header-label-icon ag-sort-ascending-icon" aria-hidden="true"></span>
+             <span data-ref="eSortDesc" class="ag-header-icon ag-header-label-icon ag-sort-descending-icon" aria-hidden="true"></span>
+             <span data-ref="eSortNone" class="ag-header-icon ag-header-label-icon ag-sort-none-icon" aria-hidden="true"></span>
          </div>
      );
  };
