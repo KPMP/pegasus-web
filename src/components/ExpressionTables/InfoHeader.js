7fe457089432158f31ea23c56e245e06c861211f
Becky Reamy <rlreamy@umich.edu>
2025-07-29 15:53:35 -0400

    KPMP-6151: Migrating diffex by cluster table

--- src/components/ExpressionTables/InfoHeader.js
+++ src/components/ExpressionTables/InfoHeader.js
@@ -1,39 +1,87 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faInfoCircle, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+// import React, { useEffect, useRef, useState } from 'react';
+
+// export default (props) => {
+//     let sortOrder = props.defaultSort; 
+//     let sortArrow = setSortArrow();
+
+//     const setSortArrow = () => {
+//         if (sortOrder === 'asc') {
+//             sortArrow = 
+//                 <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortUp' icon={faArrowUp} /></span>;
+//         } else if (sortOrder === 'desc') {
+//             sortArrow =
+//                 <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortDown' icon={faArrowDown} /></span>;
+//         }
+//     }
+
+//     const onSortChanged = () => {
+//         setSortArrow();
+//     }
+
+//     const onSortRequested = (event) => {
+//         console.log("sorting")
+//         if (sortOrder === 'asc') {
+//             sortOrder = 'desc'
+//         } else {
+//             sortOrder = 'asc'
+//         }
+//     };
+    
+//     useEffect(() => {
+//         props.column.addEventListener('sortChanged', onSortChanged);
+//         onSortChanged();
+//     }, []);
+    
+//     let headerIcon = null;
+//     if (props.infoIcon && props.infoIcon === true) {
+//         headerIcon = (
+//             <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='fold-change-info' icon={faInfoCircle} /></span>
+//         )
+//     }
+
+    
+//     if (props.enableSorting) {
+        
+//         return (
+//             <div>    
+//                 <div onClick={(event) => onSortRequested(event)} className="customHeaderLabel"> {sortArrow} {props.displayName} {headerIcon}</div>
+//             </div>
+//         )
+//     } else {
+//     return (
+//         <div>
+//             <div className="customHeaderLabel">{props.displayName} {headerIcon}</div>
+//         </div>);
+//     }
+// };
+
+import React from 'react';
 import { faInfoCircle, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import React, { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default (props) => {
-    let sortOrder = props.defaultSort; 
-    let sortArrow = setSortArrow();
+const InfoHeader = (props) => {
+    const { displayName, enableSorting, column, setSort } = props;
+    const sortState = column.getSort();
 
-    const setSortArrow = () => {
-        if (sortOrder === 'asc') {
-            sortArrow = 
-                <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortUp' icon={faArrowUp} /></span>;
-        } else if (sortOrder === 'desc') {
-            sortArrow =
-                <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortDown' icon={faArrowDown} /></span>;
-        }
+    const toggleSort = () => {
+        const nextSort = sortState === 'asc' ? 'desc' : sortState === 'desc' ? null : 'asc';
+        setSort(nextSort, false);
     }
 
-    const onSortChanged = () => {
-        setSortArrow();
-    }
-
-    const onSortRequested = (event) => {
-        console.log("sorting")
-        if (sortOrder === 'asc') {
-            sortOrder = 'desc'
-        } else {
-            sortOrder = 'asc'
+    const getSortArrow = () => {
+        if (sortState === 'asc') {
+            return ( 
+                <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortUp' icon={faArrowUp} /></span>
+            )
+        } else if (sortState === 'desc') {
+            return (
+                <span className="icon-info"><FontAwesomeIcon className='kpmp-light-blue' id='sortDown' icon={faArrowDown} /></span>
+            );
         }
     };
-    
-    useEffect(() => {
-        props.column.addEventListener('sortChanged', onSortChanged);
-        onSortChanged();
-    }, []);
-    
+
     let headerIcon = null;
     if (props.infoIcon && props.infoIcon === true) {
         headerIcon = (
@@ -41,18 +89,18 @@ export default (props) => {
         )
     }
 
-    
-    if (props.enableSorting) {
-        
-        return (
-            <div>    
-                <div onClick={(event) => onSortRequested(event)} className="customHeaderLabel"> {sortArrow} {props.displayName} {headerIcon}</div>
-            </div>
-        )
-    } else {
     return (
-        <div>
-            <div className="customHeaderLabel">{props.displayName} {headerIcon}</div>
-        </div>);
-    }
-};
+        <div style={{ display: 'flex', alignItems: 'center', cursor: enableSorting ? 'pointer' : 'default' }} onClick={enableSorting ? toggleSort : undefined}>
+        <span style={{ marginRight: 4 }}>{getSortArrow()}</span>
+        <span>{displayName}</span>
+        {props.colDef.info && (
+            <span style={{ marginLeft: 4 }} title={props.colDef.info}>
+            {headerIcon}
+            </span>
+        )}
+        </div>
+    );
+
+}
+
+export default InfoHeader;
