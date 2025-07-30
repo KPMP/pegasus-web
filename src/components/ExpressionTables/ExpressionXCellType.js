b53011c3a136be5379f03ba7505e90ee1e0756f6
Becky Reamy <rlreamy@umich.edu>
2025-07-30 11:42:00 -0400

    KPMP-6151: merged

--- src/components/ExpressionTables/ExpressionXCellType.js
+++ src/components/ExpressionTables/ExpressionXCellType.js
@@ -76,60 +76,60 @@ class ExpressionXCellType extends Component {
                 field: 'cluster',
                 width: 106
             },
-            {
-                headerName: 'CELL CLUSTER (<i>predicted state</i>)',
-                field: 'clusterName',
-                cellRenderer: row => this.parseClusterName(row.value),
-                width: 500
+            // {
+            //     headerName: 'CELL CLUSTER (<i>predicted state</i>)',
+            //     field: 'clusterName',
+            //     cellRenderer: row => this.parseClusterName(row.value),
+            //     width: 500
                 
-            },
-            {
-                headerName: <span># CELLS IN<br />CELL CLUSTER</span>,
-                field: 'cellCount',
-                valueFormatter: row => row.value ? row.value : 0,
-                width: 110
-            },
-            {
-                headerName: 'MEAN EXPRESSION',
-                headerComponent: CustomHeader,
-                headerTooltip: 'Averaged expression values (logarithmic) for each cell cluster',
-                field: 'avgExp',
-                valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
-                width: 125
-            },
-            {
-                headerName: '% CELLS EXPRESSING',
-                field: 'pct1',
-                valueFormatter: row => {
-                    let newValue = (row.value > 0) ? (row.value * 100) : row.value;
-                    return formatNumberToPrecision(newValue, 3, false, this.props.dataType, this.props.enrollmentCategory);
-                },
-                width: 106
-            },
-            {
-                headerName: 'FOLD CHANGE', 
-                headerComponent: CustomHeader,
-                headerTooltip: 'Log fold-change of the average expression between this cell cluster and all others. Positive values indicate that the feature is more highly expressed in this cell cluster.',
-                field: 'foldChange',
-                valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
-                width: 100
-            },
-            {
-                headerName: 'P VALUE',
-                headerComponent: CustomHeader,
-                headerTooltip: 'p-value (unadjusted)',
-                field: 'pVal',
-                valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
-                width: 106
-            },
-            {
-                headerName: 'ADJ P VALUE',
-                headerComponent: CustomHeader,
-                headerTooltip: 'Adjusted p-value, based on bonferroni correction using all features in the dataset.',
-                field: 'pValAdj',
-                valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
-                width: 100
-            }
+            // },
+            // {
+            //     headerName: <span># CELLS IN<br />CELL CLUSTER</span>,
+            //     field: 'cellCount',
+            //     valueFormatter: row => row.value ? row.value : 0,
+            //     width: 110
+            // },
+            // {
+            //     headerName: 'MEAN EXPRESSION',
+            //     headerComponent: CustomHeader,
+            //     headerTooltip: 'Averaged expression values (logarithmic) for each cell cluster',
+            //     field: 'avgExp',
+            //     valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
+            //     width: 125
+            // },
+            // {
+            //     headerName: '% CELLS EXPRESSING',
+            //     field: 'pct1',
+            //     valueFormatter: row => {
+            //         let newValue = (row.value > 0) ? (row.value * 100) : row.value;
+            //         return formatNumberToPrecision(newValue, 3, false, this.props.dataType, this.props.enrollmentCategory);
+            //     },
+            //     width: 106
+            // },
+            // {
+            //     headerName: 'FOLD CHANGE', 
+            //     headerComponent: CustomHeader,
+            //     headerTooltip: 'Log fold-change of the average expression between this cell cluster and all others. Positive values indicate that the feature is more highly expressed in this cell cluster.',
+            //     field: 'foldChange',
+            //     valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
+            //     width: 100
+            // },
+            // {
+            //     headerName: 'P VALUE',
+            //     headerComponent: CustomHeader,
+            //     headerTooltip: 'p-value (unadjusted)',
+            //     field: 'pVal',
+            //     valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
+            //     width: 106
+            // },
+            // {
+            //     headerName: 'ADJ P VALUE',
+            //     headerComponent: CustomHeader,
+            //     headerTooltip: 'Adjusted p-value, based on bonferroni correction using all features in the dataset.',
+            //     field: 'pValAdj',
+            //     valueFormatter: row => formatNumberToPrecision(row.value, 3, false, this.props.dataType, this.props.enrollmentCategory),
+            //     width: 100
+            // }
         ]
     };
 
