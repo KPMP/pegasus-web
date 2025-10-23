3bfc77c5ebd2d7c6e27ee280f5b7efb2b0b4b302
Becky Reamy <rlreamy@umich.edu>
2025-10-23 14:24:45 -0400

    KPMP-6277: Go to celltypesummary2 on search

--- src/components/ConceptSelect/ConceptSelect.js
+++ src/components/ConceptSelect/ConceptSelect.js
@@ -52,7 +52,7 @@ class ConceptSelect extends Component {
                             }
                             let hasResultsWithDataType = this.props.dataType ? results.dataTypesForConcept.includes(this.props.dataType) : true;
                             if (hasResults && hasResultsWithDataType) {
-                                this.props.setSelectedConcept(selected.value);
+                                this.props.setSelectedConcept(selected.value, this.featureNewCellClusterData);
                                 this.setState({ value: { label: selected.value.value, value: selected.value }, hasResults, hasResultsWithDataType, noResultValue: '', alertVisible: false });
                             } else {
                                 this.setState({ value: { label: selected.value.value, value: selected.value }, hasResults, hasResultsWithDataType, noResultValue: selected.value.value, alertVisible: true });
@@ -65,7 +65,7 @@ class ConceptSelect extends Component {
                     );
                 }
             } else {
-                this.props.setSelectedConcept(selected.value);
+                this.props.setSelectedConcept(selected.value, this.props.featureNewCellClusterData);
                 this.setState({ value: { label: selected.value.value, value: selected.value }, hasResults: true, noResultsValue: '', alertVisible: false });
             }
             ReactGA4.event({
