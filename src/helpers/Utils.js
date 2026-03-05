6fa2a85d0c3d90ee2215361795cb44912ad871de
rlreamy <34109594+rlreamy@users.noreply.github.com>
2026-03-05 15:51:51 -0500

    Update Utils.js

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -184,6 +184,5 @@ export const availableDataVisibilityFilter = (data) => {
     return undefined;
 }
 
-export const isSpatialData = (dataType) => {
-    const spatialDataTypes = ["Spatial Metabolomics"];
-}
+
+
