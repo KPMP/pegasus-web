b1c50547250fa498aa0bf7e1fb6b1d83d5ce10ca
HaneenT <haneen@umich.edu>
2026-06-25 12:49:12 -0400

    KPMP-6733: Update schematic citation

--- src/components/Explorer/AccordionTabSection.js
+++ src/components/Explorer/AccordionTabSection.js
@@ -131,7 +131,7 @@ class AccordionTabSection extends Component {
                 </Row>
                <Row className="citation">
                     <Col>
-                        <small>Schematics courtesy of Bajema, Rachel, Supriya Bidanta, Ellen M. Quardokus, Bruce W. Herr II, and Katy Börner. December 2025. https://humanatlas.io/2d-ftu-illustrations, Accessed on June 1, 2026.</small>
+                        <small>Functional Tissue Unit (FTU) Illustration courtesy of Rachel Bajema, Supriya Bidanta, Ellen M. Quardokus, Bruce W. Herr II, Katy B&#246;rner, and kidney domain expert Sanjay Jain. December 2025. <a href='https://humanatlas.io/2d-ftu-illustrations?releaseVersion=2.5' target='_blank'>https://humanatlas.io/2d-ftu-illustrations</a>, Accessed on June 1, 2026.</small>
                     </Col>
                 </Row>
             </TabPane >
